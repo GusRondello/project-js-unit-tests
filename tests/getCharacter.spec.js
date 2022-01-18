bb2b30109ad09d4e4aeb966144e75d59723362de
Gustavo Rondello <gustavorondello99@outlook.com>
2022-01-18 18:39:23 -0300

    Requisito 9 completo

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -41,15 +41,44 @@ Retorno:
 }
 */
 
-describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
-  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
+const characters = {
+  arya: {
+    name: 'Arya Stark',
+    class: 'Rogue',
+    phrases: ['Not today', 'A girl has no name.'],
+  },
+  brienne: {
+    name: 'Brienne Tarth',
+    class: 'Knight',
+    phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'],
+  },
+  melissandre: {
+    name: 'Melissandre',
+    class: 'Necromancer',
+    phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'],
+  },
+};
+
+
+describe('9 - Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
+    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
-    // Teste se os parâmetros não são Case Sensitive.
-    // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+  it('Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.', () => {
+    expect(getCharacter()).toBeUndefined();
   });
+  it('Teste se a função retorna o objeto correto para o parâmetro `Arya`', () => {
+    expect(getCharacter('arya')).toEqual(characters.arya);
+  })
+  it('Teste se a função retorna o objeto correto para o parâmetro `Brienne`', () => {
+    expect(getCharacter('brienne')).toEqual(characters.brienne);
+  })
+  it('Teste se a função retorna o objeto correto para o parâmetro `Melissandre`', () => {
+    expect(getCharacter('melissandre')).toEqual(characters.melissandre);
+  })
+  it('Teste se os parâmetros não são Case Sensitive.', () => {
+    expect(getCharacter('arya'.toUpperCase())).toEqual(characters.arya);
+  })
+  it('Teste se ao passar um nome que não está na tabela, a função retorna undefined.', () => {
+    expect(getCharacter('Jon Snow')).toBeUndefined();
+  })
 });
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -40,22 +40,26 @@ const difObj = () => {
   return false;
 }
 
-describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+describe('6 - Verifica se a função `productDetails` tem o comportamento esperado', () => {
     // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
+  it('Teste se productDetails é uma função.', () => {
     expect(typeof productDetails).toBe('function');
-    // Teste se o retorno da função é um array.
-    /*Tive ajuda da Ana na mentoria e do Leo Araujo para entender a questão do array retornar como object*/
+  });
+  it('Teste se o retorno da função é um array.', () => {
+    // Tive ajuda da Ana na mentoria e do Leo Araujo para entender a questão do array retornar como object
     expect(Array.isArray(productDetails())).toBeTruthy();
-    // Teste se o array retornado pela função contém dois itens dentro.
+  });
+  it('Teste se o array retornado pela função contém dois itens dentro.', () => {
     expect(productDetails().length).toBe(2);
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
+  });
+  it('Teste se os dois itens dentro do array retornado pela função são objetos.', () => {
     expect(typeof productDetails()).toBe('object');
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+  });
+  it('Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.', () => {
     expect(difObj()).toBeTruthy();
-    // Teste se os dois productIds terminam com 123.
+  });
+  it('Teste se os dois productIds terminam com 123.', () => {
     expect(object1Id && object2Id).toContain(Id);
   });
 });
