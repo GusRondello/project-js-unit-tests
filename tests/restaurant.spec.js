e9d3f9a2e8d7d4e73d0899fa56f48d5c0668d6e1
Gustavo Rondello <gustavorondello99@outlook.com>
2022-01-20 20:34:20 -0300

    resetado requisito 10

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -44,22 +44,25 @@ const createMenu = require('../src/restaurant');
 
 */
 
-describe('10 -Verifica se a função `createMenu` tem o comportamento esperado', () => {
-  // fail('Teste vazio!');
-  it('TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui a chave fetchMenu, a qual tem como valor uma função.', () => {
-    const objetoRetornado = createMenu(); 
-    expect()
-  });
-  it('TESTE 2: Verifique se `objetoRetornado.fetchMenu()` retorna um objeto cujas chaves são somente `food` e `drink`, considerando que a função createMenu() foi chamada com o objeto: `{ food: {}, drink: {} }`.', () => {
-    const objetoRetornado = createMenu({ food: {}, drink: {} });
+describe('10 - Implemente os casos de teste e a função `createMenu`', () => {
+  it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
+    // fail('Teste vazio!');
+    // TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui a
+    // chave fetchMenu, a qual tem como valor uma função.
+    // ```
+    // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
+    // ```
+    // TESTE 2: Verifique se 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`, 
+    // considerando que a função createMenu() foi chamada com o objeto: `{ food: {}, drink: {} }`.
+    // ```
+    // const objetoRetornado = createMenu({ food: {}, drink: {} });
     // objetoRetornado.fetchMenu() // Retorno: { food: {}, drink: {}}
     // ```
-  });
-  it ('TESTE 3: Verifique se o menu passado pra função createMenu é identico ao menu recuperado pela função `objetoRetornado.fetchMenu`', () => {
-    const objetoRetornado = createMenu(objetoQualquer);
+    // TESTE 3: Verifique se o menu passado pra função createMenu é identico ao menu recuperado pela função 'objetoRetornado.fetchMenu'
+    // ```
+    // const objetoRetornado = createMenu(objetoQualquer);
     // objetoRetornado.fetchMenu() // Retorno: objetoQualquer
     // ```
-  });
     // Agora faça o PASSO 1 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
     // TESTE 4: Verifique se 'objetoRetornado.consumption', após a criação do menu, retorna um array vazio.
@@ -106,4 +109,5 @@ describe('10 -Verifica se a função `createMenu` tem o comportamento esperado',
     // objetoRetornado.pay() // Retorno: somaDosPreçosDosPedidos
     // ```
     // Agora faça o PASSO 4 no arquivo `src/restaurant.js`.
+  });
 });
